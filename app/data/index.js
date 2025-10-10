29846f735c4be5f9c9498c684158cda88ceeceda
Katy248 <petrovanton247@gmail.com>
2025-10-10 12:49:29 +0300

    Added data

---
+++ app/data/index.js
@@ -0,0 +1,4 @@
+export const PHONE_NUMBER = "+7 (495) 120-04-85";
+export const PHONE_NUMBER_NORMALIZED = "+74951200485";
+export const ADDRESS = "г. Москва, пр-кт Волгоградский, д. 32, кор 31";
+export const EMAIL = "info@icentr.pro";
