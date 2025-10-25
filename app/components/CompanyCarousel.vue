f4fd1db3c62511a355156b51b3901f4a84f843c6
Katy248 <petrovanton247@gmail.com>
2025-10-25 19:28:53 +0300

    Added buttons

--- app/components/CompanyCarousel.vue
+++ app/components/CompanyCarousel.vue
@@ -1,9 +1,14 @@
 <script setup>
+import { ref, computed, onMounted, onUnmounted } from "vue";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/vue/24/outline";
+
 import Company1 from "@/assets/Company1.jpg";
 import Company2 from "@/assets/Company2.jpg";
 import Company3 from "@/assets/Company3.jpg";
 import Company4 from "@/assets/Company4.jpg";
 import Company5 from "@/assets/Company5.jpg";
+
+const SLIDE_TIMEOUT = 10_000;
 
 const slides = [
     {
@@ -34,26 +39,80 @@ const slides = [
 ];
 
 const currentSlideIndex = ref(0);
+let autoPlayInterval = null;
 
 const gotoSlide = (slide) => {
     currentSlideIndex.value = slides.indexOf(slide);
 };
 
+const nextSlide = () => {
+    currentSlideIndex.value = (currentSlideIndex.value + 1) % slides.length;
+};
+
+const prevSlide = () => {
+    currentSlideIndex.value = (currentSlideIndex.value - 1 + slides.length) % slides.length;
+};
+
+const startAutoPlay = () => {
+    autoPlayInterval = setInterval(nextSlide, SLIDE_TIMEOUT);
+};
+
+const stopAutoPlay = () => {
+    if (autoPlayInterval) {
+        clearInterval(autoPlayInterval);
+        autoPlayInterval = null;
+    }
+};
+
 const currentSlide = computed(() => slides[currentSlideIndex.value]);
+
+// Запускаем автоматическое пролистывание при монтировании компонента
+onMounted(() => {
+    startAutoPlay();
+});
+
+// Останавливаем при размонтировании
+onUnmounted(() => {
+    stopAutoPlay();
+});
 </script>
+
 <template>
-    <div class="flex flex-col gap-4">
-        <div class="flex justify-center">
-            <img :src="currentSlide.image" alt="" />
+    <div class="flex flex-col gap-4 relative">
+        <!-- Основное изображение с кнопками -->
+        <div class="flex justify-center relative group">
+            <img :src="currentSlide.image" :alt="currentSlide.alt" class="transition-opacity duration-300" @mouseenter="stopAutoPlay" @mouseleave="startAutoPlay" />
+
+            <!-- Кнопка "Предыдущий" -->
+            <button
+                @click="prevSlide"
+                class="absolute left-4 top-1/2 transform -translate-y-1/2 bg-white/80 hover:bg-white p-2 rounded-full shadow-lg transition-all opacity-0 group-hover:opacity-100"
+                @mouseenter="stopAutoPlay"
+                @mouseleave="startAutoPlay">
+                <ChevronLeftIcon class="w-6 h-6 text-gray-800" />
+            </button>
+
+            <!-- Кнопка "Следующий" -->
+            <button
+                @click="nextSlide"
+                class="absolute right-4 top-1/2 transform -translate-y-1/2 bg-white/80 hover:bg-white p-2 rounded-full shadow-lg transition-all opacity-0 group-hover:opacity-100"
+                @mouseenter="stopAutoPlay"
+                @mouseleave="startAutoPlay">
+                <ChevronRightIcon class="w-6 h-6 text-gray-800" />
+            </button>
         </div>
+
+        <!-- Миниатюры -->
         <div class="flex gap-2 justify-center">
             <img
-                @click="() => gotoSlide(slide)"
-                class="w-20 h-20 rounded-md border-2"
+                @click="gotoSlide(slide)"
+                class="w-20 h-20 rounded-md border-2 cursor-pointer transition-all hover:scale-105"
                 v-for="slide in slides"
                 :src="slide.image"
                 :alt="slide.alt"
-                :class="currentSlide == slide ? 'border-black' : 'border-white'" />
+                :class="currentSlide == slide ? 'border-black scale-110' : 'border-white'"
+                @mouseenter="stopAutoPlay"
+                @mouseleave="startAutoPlay" />
         </div>
     </div>
 </template>
