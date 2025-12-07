15548d343de1cf0e60066febdf7865ba51c65b85
Katy248 <petrovanton247@gmail.com>
2025-12-08 00:29:44 +0300

    Added logging

--- app/plugins/api.client.js
+++ app/plugins/api.client.js
@@ -3,5 +3,6 @@ import { createApi } from "@/api";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
+  console.log("Using base URL for API: " + config.public.apiBase);
   createApi(config.public.apiBase);
 });
